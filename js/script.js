92170c2d33a77d6ebca396d7fc3065783311c600
LauraCavo <laura.cavo@outlook.com>
2023-04-03 11:36:19 -0300

    modificación archivo js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const oculto_btn = document.getElementById("oculto_btn");
-const close_img = document.getElementById("close_img");
-
-function oculto_btn() {
-  oculto_btn.classList.remove("ocultar");
+function mostrar() {
+  const oculto_btn = document.getElementById("oculto_btn");
+  oculto_btn.classList.remove(".mostrar");
 }
+
 function close_img() {
-  close_img.classList.add("ocultar");
+  const close_btn = document.getElementById("close_btn");
+  close_btn.classList.add(".mostar");
 }
