0793320baa1735c61e9e8f82126e9bfe1b047121
LauraCavo <laura.cavo@outlook.com>
2023-04-03 00:10:28 -0300

    se crea archivo js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const oculto_btn = document.getElementById("oculto_btn");
+const close_img = document.getElementById("close_img");
+
+function oculto_btn() {
+  oculto_btn.classList.remove("ocultar");
+}
+function close_img() {
+  close_img.classList.add("ocultar");
+}
